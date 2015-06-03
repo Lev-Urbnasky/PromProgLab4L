749d22607501c9c380750f89cd60e6b9a95dfb58
Lev-Urbansky <nickolas.konnors@gmail.com>
2015-06-03 13:37:27 +0300

    Area is now computing area

--- src/Area.java
+++ src/Area.java
@@ -2,5 +2,22 @@
  * Created by Лев on 03.06.2015.
  */
 public class Area {
+    private double X;
+    private double Y;
+    private double Z;
 
+    public Area(double X, double Y, double Z) {
+        this.X = X;
+        this.Y = Y;
+        this.Z = Z;
+    }
+
+    public double getArea() {
+        return X * Y * Z;
+    }
+
+    public static void main(String[] args) {
+        Area area = new Area(11.4, 18.6, 16.7);
+        System.out.println(area.getArea());
+    }
 }
